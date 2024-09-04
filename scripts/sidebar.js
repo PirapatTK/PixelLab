7e72aaa3ef9e2ed81118894b8a9573512cae6571
pixel <pixel@odds.team>
2024-09-04 08:39:23 +0700

    chore: Refactor sidebar.js for improved responsiveness and code organization

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -16,23 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
             sideBarSize = "250px";
         }
 
-        if (sideBar.offsetWidth > 0) {  // Check if sidebar is visible
-            sideBar.style.width = sideBarSize;
-            contentBody.style.marginLeft = window.innerWidth < 768 ? "0px" : sideBarSize;
-            contentBody.style.filter = window.innerWidth < 768 ? "brightness(0.5)" : "none";
-            contentBody.style.pointerEvents = window.innerWidth < 768 ? "none" : "auto"; // Disable interactions
-            document.body.style.overflow = window.innerWidth < 768 ? "hidden" : "auto"; // Disable scrolling
+        if (window.innerWidth < 768) {
+            closeNav();
+        } else {
+            openNav();
         }
     }
 
     function toggleSideBar() {
-        if (sideBar.offsetWidth === 0) {  // Check if sidebar is closed
+        if (sideBar.offsetWidth === 0) {
             openNav();
         } else {
             closeNav();
-            contentBody.style.filter = "none";
-            contentBody.style.pointerEvents = "auto"; // Enable interactions
-            document.body.style.overflow = "auto"; // Enable scrolling
         }
     }
 
@@ -47,13 +42,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function closeNav() {
         sideBar.style.width = "0";
         contentBody.style.marginLeft = "0";
+        contentBody.style.filter = "none";
         contentBody.style.pointerEvents = "auto"; // Enable interactions
         document.body.style.overflow = "auto"; // Enable scrolling
     }
 
     // Close sidebar when clicking on the content for screens smaller than 768px
     contentBody.addEventListener('click', () => {
-        if (window.innerWidth < 768 && sideBar.offsetWidth > 0) {  // Check if sidebar is open
+        if (window.innerWidth < 768 && sideBar.offsetWidth > 0) { 
             closeNav();
         }
     });
